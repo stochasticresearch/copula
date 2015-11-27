674f4c15a89415ba08be2027e8e23b780eda0367
Kiran Karra <kiran.karra@gmail.edu>
2015-11-26 22:14:32 -0500

    added test case for testing copula family generation

--- algorithms/test/hcbnTest.m
+++ algorithms/test/hcbnTest.m
@@ -113,11 +113,91 @@ classdef hcbnTest < matlab.unittest.TestCase
         end
         
         function testEstFamilyCopula(testCase)
+            % make a hybrid network as follows w/ simulated data.
+            %  A    B
+            %   \ /  \ 
+            %    C    D
+            %    |
+            %    E
+            % All arrows point downwards.  A, B, and E will be continuous
+            % nodes, C and D will be discrete nodes.  We will induce a 
+            % Gaussian dependency between the nodes. of different
+            % correlation amounts to simulate different dependencies
+            % between the nodes.
+            M = 1000;
+            D = 5;
             
-        end
-        
-        function testHcbnLogLikelihood(testCase)
+            % Generate samples from C1 (A,B,C) [Gaussian Copula]
+            Rho_C1 = [1 .4 .2; .4 1 -.8; .2 -.8 1];
+            Z = mvnrnd([0 0 0], Rho_C1, M);
+            U_C1 = normcdf(Z,0,1);
             
+            % Generate samples from C2 (B,D) [Clayton Copula]
+            U_C2_1 = U_C1(:,2); c2_alpha = 2; p = rand(M,1);
+            U_C2_2 = U_C2_1.*(p.^(-c2_alpha./(1+c2_alpha)) - 1 + U_C2_1.^c2_alpha).^(-1./c2_alpha);
+            U_C2 = [U_C2_1 U_C2_2];
+            
+            % Generate samples from C3 (C,E) [Clayton Copula]
+            U_C3_1 = U_C1(:,3); c3_alpha = 4; p = rand(M,1);
+            U_C3_2 = U_C3_1.*(p.^(-c3_alpha./(1+c3_alpha)) - 1 + U_C3_1.^c3_alpha).^(-1./c3_alpha);
+            U_C3 = [U_C3_1 U_C3_2];
+            
+            U = [U_C1 U_C2(:,2) U_C3(:,2)];
+            
+            X = [gaminv(U(:,1),2,1) ...
+                   betainv(U(:,2),2,2) ...
+                   unidinv(U(:,3),5) ...
+                   unidinv(U(:,4),3) ...
+                   norminv(U(:,5),0,1)];
+                        
+            nodes = {'A', 'B', 'C', 'D', 'E'};
+            discreteNodes = {'C', 'D'};
+            
+            % make a dag that is correct (not acyclic)
+            dag = zeros(D,D);
+            aa = 1; bb = 2; cc = 3; dd = 4; ee = 5;
+            dag(aa,cc) = 1;
+            dag(bb,[cc dd]) = 1;
+            dag(cc, ee) = 1;
+            hcbnObj = hcbn(testCase.bntPath, X, nodes, discreteNodes, dag);
+            
+            % ensure that we have no families for the root nodes
+            A_family_expect = []; A_family_actual = hcbnObj.copulaFamilies{1};
+            B_family_expect = []; B_family_actual = hcbnObj.copulaFamilies{2};
+            testCase.verifyEqual(A_family_actual, A_family_expect);
+            testCase.verifyEqual(B_family_actual, B_family_expect);
+            
+            % we compare the empirically calculated copulas to the known
+            % form values
+            D_family_actual = hcbnObj.copulaFamilies{4}.C;
+            D_family_U = hcbnObj.copulaFamilies{4}.U;
+            D_family_expect = zeros([size(D_family_U,1) size(D_family_U,2)]);
+            for ii=1:length(D_family_expect)
+                for jj=1:length(D_family_expect)
+                    u = [D_family_U(ii,jj,1) D_family_U(ii,jj,2)];
+                    D_family_expect(ii,jj) = copulacdf('Clayton', u, c2_alpha);
+                end
+            end
+            subplot(1,2,1); contour(D_family_U(:,:,1),D_family_U(:,:,2), D_family_actual);
+            grid on; title('Empirical C2')
+            subplot(1,2,2); contour(D_family_U(:,:,1),D_family_U(:,:,2), D_family_expect);
+            grid on; title('Theoretical C2');
+            pause;
+            
+            E_family_actual = hcbnObj.copulaFamilies{5}.C;
+            E_family_U = hcbnObj.copulaFamilies{5}.U;
+            E_family_expect = zeros([size(E_family_U,1) size(E_family_U,2)]);
+            for ii=1:length(E_family_expect)
+                for jj=1:length(E_family_expect)
+                    u = [E_family_U(ii,jj,1) E_family_U(ii,jj,2)];
+                    E_family_expect(ii,jj) = copulacdf('Clayton', u, c3_alpha);
+                end
+            end
+            subplot(1,2,1); contour(E_family_U(:,:,1),E_family_U(:,:,2), E_family_actual);
+            grid on; title('Empirical C3')
+            subplot(1,2,2); contour(E_family_U(:,:,1),E_family_U(:,:,2), E_family_expect);
+            grid on; title('Theoretical C3');
+            pause;
         end
         
         function testCopulall(testCase)
@@ -128,10 +208,13 @@ classdef hcbnTest < matlab.unittest.TestCase
             
         end
         
+        function testHcbnLogLikelihood(testCase)
+            
+        end
+        
         function testLearnStruct_hc(testCase)
             
         end
     end
-    
 end
 
