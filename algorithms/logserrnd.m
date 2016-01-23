befe2d1579b8edc418f804f6b8324fd99ed1910d
Kiran Karra <kiran.karra@gmail.edu>
2016-01-22 23:50:29 -0500

    added test script for multivariate archimedean copula rv generation

--- algorithms/logserrnd.m
+++ algorithms/logserrnd.m
@@ -1,9 +1,12 @@
-function [ y ] = logserrnd( p, n )
+function [ y ] = logserrnd( p, n, varargin )
 %LOGSERRND generates samples from the Log-Series discrete distribution with
 %parameter p
 % Inputs:
 %  p - shape parameter of log-series distribution
 %  n - the number of random variates to generate
+% Optional Inputs:
+%  K - the number of discrete outcomes to approximate to.  The larger the
+%      better, defaults to 20
 % Outputs:
 %  y - the random variates
 
@@ -15,7 +18,7 @@ nVarargs = length(varargin);
 if(nVarargs==1)
     K = varargin{1};
     % validate it is an integer below 50
-    if(~isnumeric(K) || K>20)
+    if(~isinteger(K) || K>20 || K<0)
         warning('Invalid varargin{1}, defaulting to K=20');
         K = 20;
     end
---
+++ algorithms/test/test_mvArchimedeanCopulas.m
@@ -0,0 +1,23 @@
+% a test script for testing the multivariate (N>2) versions of generating
+% random variates from the Frank, Gumbel, and Clayton copulas
+
+M = 500;
+N = 3;
+
+alpha = 1;
+
+U_clayton = claytoncopularnd(M,N,alpha);
+U_frank   = frankcopularnd(M,N,alpha);
+U_gumbel  = gumbelcopularnd(M,N,alpha);
+
+figure;
+plotmatrix(U_clayton);
+title('Multivariate Clayton Copula')
+
+figure;
+plotmatrix(U_frank);
+title('Multivariate Frank Copula')
+
+figure;
+plotmatrix(U_gumbel);
+title('Multivariate Gumbel Copula')
