efb95aac4be5d0d27dc107d5766b9ee1d7bc0aac
Kiran Karra <kiran.karra@gmail.com>
2016-04-07 14:18:30 -0400

    updated logserrnd to deal w/ large alpha inputs

--- algorithms/logserrnd.m
+++ algorithms/logserrnd.m
@@ -16,48 +16,8 @@
 %* along with this program.  If not, see <http://www.gnu.org/licenses/>.
 %**************************************************************************
 
-function [ y ] = logserrnd( n, p, varargin )
-%LOGSERRND generates samples from the Log-Series discrete distribution with
-%parameter p - approximated as a multinomial distribution
-% Inputs:
-%  n - the number of random variates to generate
-%  p - shape parameter of log-series distribution
-% Optional Inputs:
-%  K - the number of discrete outcomes to approximate to.  The larger the
-%      better, defaults to 1000
-% Outputs:
-%  y - the random variates
 
-% We approximate the log-series distribution by converting it to a multinomial
-% distribution with K = 1000 (default)
-K = 1000;
-
-nVarargs = length(varargin);
-if(nVarargs==1)
-    K = varargin{1};
-    % validate it is an integer below 2000
-    if(~isnumeric(K) || K>2000 || K<0)
-        warning('Invalid varargin{1}, defaulting to K=1000');
-        K = 1000;
-    end
-end
-
-probvec = zeros(1,K);
-for kk=1:K
-    probvec(kk) = - p^kk ./ (kk*log1p(-p));
-end
-
-% normalize probvec to sum to 1
-probvec = probvec/sum(probvec);
-
-pd = makedist('Multinomial','Probabilities',probvec);
-
-% generate random variates from this created probability distribution
-y = random(pd, n, 1);
-
-end
-
-function [ y ] = logserrnd_R_impl(n, p)
+function [ y ] = logserrnd(n, p)
 % Performs the same as LOGSERRND above, but this is the way it was
 % implemented in the copula package in R.
 
