9bab5133ca36a222f003cfd208fd82b461f30e45
Kiran Karra <kiran.karra@gmail.com>
2016-06-19 18:48:24 -0400

    updated more tests

--- startup.m
+++ startup.m
@@ -1,2 +1,25 @@
-addpath(genpath('.'))
-addpath(genpath('../minepy/matlab'));
+% avoid adding all the git directories to the path just by doing it
+% manually for now ...
+addpath('./algorithms');
+addpath('./algorithms/test');
+addpath('./algorithms/mex');
+
+addpath('./bn');
+addpath('./bn/test');
+
+addpath('./data');
+addpath('./data/test');
+
+addpath('./misc');
+
+addpath('./optim');
+
+addpath('./simulations');
+addpath('./simulations/betakernel');
+addpath('./simulations/cos');
+addpath('./simulations/hcbn');
+addpath('./simulations/misc');
+
+addpath('./testfiles');
+
+addpath('../minepy/matlab');
