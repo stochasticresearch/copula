cc78ba67dbb720d0270d42f678482f6c44a62d3a
Kiran Karra <kiran.karra@gmail.com>
2016-10-14 08:06:30 -0400

    added code to be able to call R from unix and mac

--- startup.m
+++ startup.m
@@ -50,3 +50,37 @@ for ii=1:length(requiredModules)
         end
     end
 end
+
+% setup R BATCH commands to be callable from Matlab
+PATH = getenv('PATH');
+z = strsplit(PATH,':');
+
+MAC_R_PATH = '/usr/local/bin';
+UNIX_R_PATH = '/usr/bin';
+WINDOWS_R_PATH = ''; % ?? -- prompt user?
+
+if(ismac)
+    notFound = 1;
+    for ii=1:length(z)
+        if(strcmp(z{ii},MAC_R_PATH))
+            notFound = 0;
+            break;
+        end
+    end
+    if(notFound)
+        setenv('PATH', [PATH ':' MAC_R_PATH]);
+    end
+elseif(isunix)
+    notFound = 1;
+    for ii=1:length(z)
+        if(strcmp(z{ii},UNIX_R_PATH))
+            notFound = 0;
+            break;
+        end
+    end
+    if(notFound)
+        setenv('PATH', [PATH ':' UNIX_R_PATH]);
+    end
+else
+    % TODO: add path to R in Windows installation... prompt user?
+end
