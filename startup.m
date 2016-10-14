17896a6f9e9398736a9834dc1b957c6d3d0dd7ac
Kiran Karra <kiran.karra@gmail.com>
2016-10-13 21:33:00 -0400

    added checks to python imports

--- startup.m
+++ startup.m
@@ -27,10 +27,26 @@ addpath('../minepy/matlab');
 
 addpath('./python');
 
-% setup Python in Matlab
-pyversion 2.7   % assume you have Python 2.7 :D
+% setup Python in Matlab if it isn't already loaded
+try
+    pyversion 2.7   % assume you have Python 2.7 :D
+catch
+end
 
 % add the python folder to the PYTHON search path
 if count(py.sys.path,'./python') == 0
     insert(py.sys.path,int32(0),'./python');
+end
+
+% python module dependencies check
+requiredModules = {'numpy', 'matplotlib', 'scipy', 'sklearn', 'cvxopt'};
+for ii=1:length(requiredModules)
+    modName = requiredModules{ii};
+    try
+        py.importlib.import_module(modName);
+    catch ME
+        if (strcmp(ME.identifier,'MATLAB:Python:PyException'))
+            warning('Python Error! module: %s required for python extension modules!', modName);
+        end
+    end
 end
